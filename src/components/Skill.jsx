f64798a22ca2e1fd46059508ebc80d3c508d72c8
NakulanT <nakult721@gmail.com>
2024-02-06 21:10:52 +0530

    Added sliding image

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,10 +1,24 @@
+// Skill.jsx
+
 import React from "react";
+import a from '../images/sam1.jpeg';
+import b from '../images/sam2.png';
+import c from '../images/sam3.png';
+import './Skill.css';
 
 const Skill = () => {
+    const images = [a, b, c];
+
     return (
         <div>
             <h1>Skill</h1>
-            <div className="skill_set"></div>
+            <div className="slideshow">
+                <div className="slides">
+                    {images.map((image, index) => (
+                        <img key={index} src={image} alt={`Slide ${index}`} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
