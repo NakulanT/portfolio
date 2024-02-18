87e5b3ae400def798b33a6ca4a3cce616ad3e7dd
NakulanT <nakult721@gmail.com>
2024-02-18 20:51:44 +0530

    added description using chatgpt

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,11 @@ import s3 from '../images/c.png';
 
 const Project = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
-    const slides = [s1, s2, s3]; // Add your slide images here
+    const slides = [
+        { image: s1, description: "Python Programming Language" },
+        { image: s2, description: "Java Programming Language" },
+        { image: s3, description: "C Programming Language" }
+    ]; // Add your slide images and descriptions here
 
     const nextSlide = () => {
         setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
@@ -27,10 +31,13 @@ const Project = () => {
 
     return (
         <div className="Projects">
-            <div className="Projects-container">
+            <div className="Fullbox">
                 <div className="ContentBox">
                     <button className="prevButton" onClick={prevSlide}>{'<'}</button>
-                    <img src={slides[currentSlide]} alt={`Slide ${currentSlide + 1}`} />
+                    <div className="slideContainer">
+                        <img src={slides[currentSlide].image} alt={`Slide ${currentSlide + 1}`} />
+                        <div className="description">{slides[currentSlide].description} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quam quibusdam quaerat temporibus. Aliquam ipsum culpa ducimus, commodi dolor deleniti? Ratione corporis, nemo nihil porro facere quod quo totam magnam!</div>
+                    </div>
                     <button className="nextButton" onClick={nextSlide}>{'>'}</button>
                 </div>
             </div>
