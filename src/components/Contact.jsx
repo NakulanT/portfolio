823542590af741a6f4d026123baf0d3f6ad3eca4
NakulanT <nakult721@gmail.com>
2024-02-26 21:45:45 +0530

    Added css for contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Contact.css'
-import axios from "axios";
 
 const Contact = () => {
 
@@ -8,26 +7,16 @@ const Contact = () => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await axios.post('/send-email', {
-                name, email, message
-            });
-            console.log('email sented sucessfully!', response.data);
-        }
-        catch (error) {
-            console.log("Error in sending email : ", error);
-        }
-    }
+    const handleSubmit = () => {
+        console.log(name , email ,message);
+    } 
     return (
         <div className="Contact">
             <h1 id="subheadings">○ Contact</h1>
             <form onSubmit={handleSubmit}>
                 <div className="form">
                     <div className="form-name">
-                        <label htmlFor="name" className="form-name">Name </label>
+                        <label htmlFor="name">Name </label>
                         <input
                             type="text"
                             id="name"
@@ -37,7 +26,7 @@ const Contact = () => {
                         />
                     </div>
                     <div className="form-email">
-                        <label htmlFor="email" className="form-email">Email:</label>
+                        <label htmlFor="email" >Email</label>
                         <input
                             type="email"
                             id="email"
@@ -47,7 +36,7 @@ const Contact = () => {
                         />
                     </div>
                     <div className="form-message">
-                        <label htmlFor="message">Message:</label>
+                        <label htmlFor="message">Message</label>
                         <textarea
                             id="message"
                             value={message}
